666fcd4b518a15a5f7c05dfd27cb86df7f237f76
grleung <bee.leung@wisc.edu>
2026-06-11 14:41:19 -0600

    add filepath finder

---
+++ incus_track_les/__init__.py
@@ -0,0 +1,6 @@
+from .paths import MODEL_DATA_DIR, TRACK_DIR
+from .data_io import get_filepaths
+
+__all__ = [
+    "get_filepaths",MODEL_DATA_DIR,TRACK_DIR
+]
---
+++ incus_track_les/paths.py
@@ -0,0 +1,7 @@
+# incus_track_les/paths.py
+
+from pathlib import Path
+
+MODEL_DATA_DIR = Path('/monsoon/MODEL/LES_MODEL_DATA/V1')
+
+TRACK_DIR = Path ('/monsoon/MODEL/LES_MODEL_DATA/Tracking/V1.2')
